a018892db67e99fc72d74a0e4c019e30ccd6e151
LamTamNhu <pansuzu@gmail.com>
2023-09-20 16:31:28 +0700

    +bai tap 17

--- module1/17/bai_tap/move_character/script.js
+++ module1/17/bai_tap/move_character/script.js
@@ -1,26 +1,28 @@
-function Character(sprite, top, left, size) {
-    this.sprite = sprite;
-    this.top = top;
-    this.left = left;
-    this.size = size;
-
+class Character {
+    constructor(sprite, top, left, size, speed) {
+        this.sprite = sprite;
+        this.top = top;
+        this.left = left;
+        this.size = size;
+        this.speed = speed;
+    }
 }
+
 
 Character.prototype.moveCharacter = function (evt) {
     console.log(evt.key);
     switch (evt.key) {
-        case 'ArrowDown':
-            this.top -= 5;
-            break;
         case 'ArrowUp':
-            this.top += 5;
-            console.log(crying.top);
+            this.top -= this.speed;
+            break;
+        case 'ArrowDown':
+            this.top += this.speed;
             break;
         case 'ArrowLeft':
-            this.left -= 5;
+            this.left -= this.speed;
             break;
         case 'ArrowRight':
-            this.left += 5;
+            this.left += this.speed;
             break;
     }
     update();
@@ -28,18 +30,24 @@ Character.prototype.moveCharacter = function (evt) {
 }
 
 Character.prototype.setCharacter = function () {
-    return `<img src='${this.sprite}' width="${this.size}" height="${this.size}" style="top: ${this.top} ; left: ${this.left}; position:absolute;" alt="Sprite error">`
+    return `<img src="${this.sprite}" width="${this.size}" height="${this.size}" style="top: ${this.top}` + "px" + ` ; left: ${this.left}` + "px" + ` ; position:absolute;"
+     alt="Sprite error">`
 }
 
-let crying = new Character('ToT.png', 20, 30, 200);
+let crying = new Character('ToT.png', 200, 300, 200,50);
+
+// crying.moveCharacter()
 
 function update() {
     document.getElementById('game').innerHTML = crying.setCharacter();
 }
 
 function start() {
-    addEventListener('keydown', crying.moveCharacter);
+    addEventListener('keydown', crying.moveCharacter.bind(crying));
     update();
 }
+
+
+
 
 
