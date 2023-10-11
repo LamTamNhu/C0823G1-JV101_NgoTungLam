262a5651eb092a5f58830a2aba7413319a73af3d
LamTamNhu <pansuzu@gmail.com>
2023-10-11 09:54:34 +0700

    *fix PrimeUnder100.java

--- module_2/src/ss2/prac/PrimeUnder100.java
+++ module_2/src/ss2/prac/PrimeUnder100.java
@@ -3,8 +3,9 @@ package ss2.prac;
 public class PrimeUnder100 {
     public static void main(String[] args) {
         int num = 2;
+        boolean isPrime;
         while (num <= 100) {
-            boolean isPrime = true;
+            isPrime = true;
             for (int i = 2; i <= Math.sqrt(num); i++) {
                 if (num % i == 0) {
                     isPrime = false;
