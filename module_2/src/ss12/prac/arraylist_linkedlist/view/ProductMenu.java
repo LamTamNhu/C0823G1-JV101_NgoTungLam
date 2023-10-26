5f896560697eaf164642ac02f36a178e146d1410
LamTamNhu <pansuzu@gmail.com>
2023-10-26 15:18:11 +0700

    *fix menu mvc ss12

--- module_2/src/ss12/prac/arraylist_linkedlist/view/ProductMenu.java
+++ module_2/src/ss12/prac/arraylist_linkedlist/view/ProductMenu.java
@@ -6,23 +6,25 @@ import ss12.prac.arraylist_linkedlist.model.Product;
 import java.util.*;
 
 public class ProductMenu {
+    private static final ProductController productController = new ProductController();
     private static final Scanner scanner = new Scanner(System.in);
-
-    private static int input;
-
-    public static void main(String[] args) {
-        ProductController productController = new ProductController();
-        List<Product> products;
-        Integer id;
-        do {
-            System.out.println("------Product Menu------");
-            System.out.print("1. Display product list.\n" +
+    private static Integer id;
+    private static Product product;
+    private static final String menu =
+            "------Product Menu------\n" +
+                    "1. Display product list.\n" +
                     "2. Find product by name.\n" +
                     "3. Add product.\n" +
                     "4. Edit product by id.\n" +
                     "5. Remove product by id.\n" +
                     "0. Exit\n" +
-                    "Select a number: ");
+                    "Select a number: ";
+
+
+    public static void main(String[] args) {
+        do {
+            System.out.print(menu);
+            int input;
             try {
                 input = Integer.parseInt(scanner.nextLine());
             } catch (Exception e) {
@@ -31,73 +33,93 @@ public class ProductMenu {
                 continue;
             }
 
-            Product product;
             switch (input) {
                 case 1:
-                    System.out.println("-----Product List-----");
-                    products = productController.getAll();
-                    for (Product productElement : products) {
-                        System.out.println(productElement);
-                    }
-                    enterToContinue();
+                    displayProducts();
                     break;
                 case 2:
-                    System.out.print("Enter product's name: ");
-                    String productName = scanner.nextLine().toLowerCase();
-                    product = productController.findProductByName(productName);
-                    if (product != null) {
-                        System.out.println(product);
-                    } else {
-                        System.out.println("Can't find any product with name: " + productName);
-                    }
-                    enterToContinue();
+                    findProductByName();
                     break;
                 case 3:
-                    id = inputId();
-                    if (productController.checkIdInput(id)) {
-                        System.out.println("This id already existed in list!");
-                        enterToContinue();
-                        break;
-                    }
-                    product = inputInfo();
-                    product.setId(id);
-                    Boolean isSuccess = productController.addProduct(product);
-                    if (isSuccess) {
-                        System.out.println("Product added!");
-                    } else {
-                        System.out.println("Unknown error");
-                    }
-                    enterToContinue();
+                    addProduct();
                     break;
                 case 4:
-                    id = inputId();
-                    if (!productController.checkIdInput(id)) {
-                        System.out.println("Can't find any product with this id!");
-                        enterToContinue();
-                        break;
-                    }
-                    product = inputInfo();
-                    product.setId(id);
-                    if (productController.editProduct(id, product)) {
-                        System.out.println("Edit succeed!");
-                    } else {
-                        System.out.println("Unknown error!");
-                    }
-                    enterToContinue();
+                    editProduct();
                     break;
                 case 5:
-                    id = inputId();
-                    if (productController.removeProduct(id)) {
-                        System.out.println("Product removed!");
-                    } else {
-                        System.out.println("Can't find this id!");
-                    }
-                    enterToContinue();
+                    removeProduct();
                     break;
                 case 0:
                     System.exit(0);
             }
         } while (true);
+    }
+
+    private static void removeProduct() {
+        id = inputId();
+        if (productController.removeProduct(id)) {
+            System.out.println("Product removed!");
+        } else {
+            System.out.println("Can't find this id!");
+        }
+        enterToContinue();
+    }
+
+    private static void editProduct() {
+        id = inputId();
+        if (!productController.checkIdInput(id)) {
+            System.out.println("Can't find any product with this id!");
+            enterToContinue();
+            return;
+        }
+        product = inputInfo();
+        product.setId(id);
+        if (productController.editProduct(id, product)) {
+            System.out.println("Edit succeed!");
+        } else {
+            System.out.println("Unknown error!");
+        }
+        enterToContinue();
+    }
+
+    private static void addProduct() {
+        id = inputId();
+        if (productController.checkIdInput(id)) {
+            System.out.println("This id already existed in list!");
+            enterToContinue();
+            return;
+        }
+        product = inputInfo();
+        product.setId(id);
+        Boolean isSuccess = productController.addProduct(product);
+        if (isSuccess) {
+            System.out.println("Product added!");
+        } else {
+            System.out.println("Unknown error");
+        }
+        enterToContinue();
+    }
+
+    private static void findProductByName() {
+        System.out.print("Enter product's name: ");
+        String productName = scanner.nextLine().toLowerCase();
+        product = productController.findProductByName(productName);
+        if (product != null) {
+            System.out.println(product);
+        } else {
+            System.out.println("Can't find any product with name: " + productName);
+        }
+        enterToContinue();
+    }
+
+    private static void displayProducts() {
+        List<Product> products;
+        System.out.println("-----Product List-----");
+        products = productController.getAll();
+        for (Product productElement : products) {
+            System.out.println(productElement);
+        }
+        enterToContinue();
     }
 
     private static Product inputInfo() {
