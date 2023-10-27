d15f84aebe69567a67c884efe408169b6c6d1283
LamTamNhu <pansuzu@gmail.com>
2023-10-27 16:24:59 +0700

    +ss16 practice.

---
+++ module_2/src/ss16/prac/copy_file_text/CopyFileTest.java
@@ -0,0 +1,7 @@
+package ss16.prac.copy_file_text;
+
+public class CopyFileTest {
+    public static void main(String[] args) {
+        FileTextCopier.copyFileText("src/ss16/prac/copy_file_text/from.txt","src/ss16/prac/copy_file_text/to.txt");
+    }
+}
